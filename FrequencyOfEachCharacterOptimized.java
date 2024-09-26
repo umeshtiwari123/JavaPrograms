cf697b35839cd604ab7b182235bf97c3c0734500
Umesh Tiwari <umesh.u.tiwari@gmail.com>
2024-09-26 18:16:32 +0530

    Create FrequencyOfEachCharacterOptimized.java

---
+++ FrequencyOfEachCharacterOptimized.java
@@ -0,0 +1,31 @@
+public class FrequencyOfEachCharacterOptimized{
+
+    static final int SIZE = 26;
+    public static void main(String[] args) {
+        
+        String str = "geeksforgeeks";
+        frequencyOfEachCharacterOptimized(str);
+    }
+
+    static void frequencyOfEachCharacterOptimized(String str){
+
+        int n = str.length();
+
+        int freq[] = new int[SIZE];
+        for(int i=0;i<n;i++)
+        {
+            freq[str.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(freq[str.charAt(i)-'a']!=0)
+            {
+                System.out.print(str.charAt(i));
+                System.out.print(freq[str.charAt(i)-'a']+" ");
+
+                freq[str.charAt(i)-'a'] = 0;
+            }
+        }
+    }
+}
